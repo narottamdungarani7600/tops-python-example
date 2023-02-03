f2be0b77a3dd82c7408f62601c17459c191db173
narottamdungarani7600 <narottamdungarani7600@gmail.com>
2023-02-03 10:50:23 +0530

    First Commit

---
+++ Sample.py
@@ -0,0 +1,21 @@
+'''
+Variable : It is a data name and is used to store the data value.
+a=10
+print(A)
+1. Must start with an alphabet
+2. Upper case & lower case are significant
+ASCII : American Stanadard Code For Information Interchange.
+3. Keyword is not allowed as a variable name.
+if=10 - wrong
+IF=10 - correct
+4. Space is not allowed between variable name.
+tops_sg_admin
+a,b,c,d
+'''
+a=float(input("Enter A:"))
+b=float(input("Enter B:"))
+c=a+b
+print("A : ",a)
+print("B : ",b)
+print("C : ",c)
+
